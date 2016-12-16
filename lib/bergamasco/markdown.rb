2e528561001419e86f6b7400f5ea442b0e2649e3
Martin Fenner <martin.fenner@datacite.org>
2016-12-16 19:35:51 +0100

    added yaml manipulations

--- lib/bergamasco/markdown.rb
+++ lib/bergamasco/markdown.rb
@@ -9,6 +9,20 @@ module Bergamasco
       [metadata, content]
     end
 
+    def self.update_yaml_frontmatter(metadata, new_metadata)
+      metadata.merge(new_metadata).compact
+    end
+
+    def self.join_yaml_frontmatter(metadata, content)
+      metadata.to_yaml + "---\n" + content
+    end
+
+    def self.update_file(file, new_metadata)
+      metadata, content = split_yaml_frontmatter(file)
+      metadata = update_yaml_frontmatter(metadata, new_metadata)
+      join_yaml_frontmatter(metadata, content)
+    end
+
     def self.render_html(text, options={})
       text = split_yaml_frontmatter(text).last if options[:skip_yaml_header]
 
--- spec/markdown_spec.rb
+++ spec/markdown_spec.rb
@@ -11,6 +11,40 @@ describe Bergamasco::Markdown do
     expect(content).to start_with("In 1998 Tim Berners-Lee coined")
   end
 
+  it 'should update yaml frontmatter' do
+    filepath = fixture_path + 'cool-dois.html.md'
+    file = IO.read(filepath)
+    new_metadata = { "doi" => "10.23725/0000-03VC"}
+    metadata, content = subject.split_yaml_frontmatter(file)
+    metadata = subject.update_yaml_frontmatter(metadata, new_metadata)
+    expect(metadata["doi"]).to eq("10.23725/0000-03VC")
+  end
+
+  it 'should update yaml frontmatter remove attribute' do
+    filepath = fixture_path + 'cool-dois.html.md'
+    file = IO.read(filepath)
+    new_metadata = { "title" => nil }
+    metadata, content = subject.split_yaml_frontmatter(file)
+    metadata = subject.update_yaml_frontmatter(metadata, new_metadata)
+    expect(metadata["title"]).to be nil
+  end
+
+  it 'should join yaml frontmatter' do
+    filepath = fixture_path + 'cool-dois.html.md'
+    file = IO.read(filepath)
+    metadata, content = subject.split_yaml_frontmatter(file)
+    updated_file = subject.join_yaml_frontmatter(metadata, content)
+    expect(updated_file).to eq(file)
+  end
+
+  it 'should update file' do
+    filepath = fixture_path + 'cool-dois.html.md'
+    file = IO.read(filepath)
+    new_metadata = { "doi" => "10.23725/0000-03VC"}
+    updated_file = subject.update_file(file, new_metadata)
+    expect(updated_file).to include("\ndoi: 10.23725/0000-03VC\n")
+  end
+
   it 'should convert markdown' do
     filepath = fixture_path + 'cool-dois.html.md'
     file = IO.read(filepath)
